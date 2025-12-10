1554ef8e5964c1bab346a64b12aad0475ac3e523
Sachin Bhawar <sachinbhavar@gmail.com>
2025-12-10 16:24:48 +0530

    update code

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,9 +2,19 @@ import React from "react";
 
 const HomePage = () => {
     return (
-        <div className="w-full min-h-screen flex flex-col items-center justify-center">
+        <div className="w-full h-[80vh] flex flex-col items-center justify-center space-y-8">
             Welcome to the Student Teacher Dashboard....
-            <p className="text-sm mt-32">Designed and developed by Sachin Bhawar..</p>
+            <p className="text-sm mb-4 italic">
+                Designed and developed by{" "}
+                <a
+                    href="https://sachinbhawar.netlify.app"
+                    target="_blank"
+                    className="text-blue-600 underline hover:text-blue-800 font-bold"
+                >
+                    Sachin Bhawar
+                </a>
+                ...
+            </p>
         </div>
     );
 };
